18d68e3fd6db79ee2bd31f0eeb72932bf67a14a8
annahico <annahico@gmail.com>
2025-11-20 15:11:58 +0100

    feat: finished 7/10 activites in numeros.js

---
+++ letras.js
@@ -0,0 +1,39 @@
+// Escribe una función que tome dos cadenas de texto como argumentos y devuelva su concatenación.
+
+
+
+// Escribe una función que tome una cadena de texto como argumento y devuelva su longitud.
+
+
+
+// Escribe una función que tome una cadena de texto como argumento y devuelva la misma cadena en mayúsculas.
+
+
+
+// Escribe una función que tome una cadena de texto como argumento y devuelva la misma cadena en minúsculas.
+
+
+
+// Escribe una función que tome una cadena de texto y un índice como argumentos, y devuelva el carácter en esa posición.
+
+
+
+// Escribe una función que tome una cadena de texto como argumento y devuelva la misma cadena invertida.
+
+
+
+// Escribe una función que tome una cadena de texto y un carácter como argumentos, y devuelva la cantidad de veces que el carácter aparece en la cadena.
+
+
+
+// Escribe una función que tome una cadena de texto como argumento y devuelva la misma cadena sin espacios en blanco.
+
+
+
+// Escribe una función que tome una cadena de texto como argumento y devuelva true si es un palíndromo (se lee igual de izquierda a derecha y de derecha a izquierda), o false si no lo es.
+
+
+
+// Escribe una función que tome una cadena de texto como argumento y devuelva la misma cadena con la primera letra de cada palabra en mayúscula.
+
+
--- numeros.js
+++ numeros.js
@@ -1,9 +1,9 @@
 // Escribe una función que tome dos números como argumentos y devuelva su suma.
-let sumar = (a , b) => a + b;
-console.log(sumar ( 4 , 9));
+let sumar = (a, b) => a + b;
+console.log(sumar(4, 9));
 
 // Escribe una función que tome dos números como argumentos y devuelva su resta.
-let restar = (a , b) => a - b;
+let restar = (a, b) => a - b;
 console.log(restar(7, 5));
 
 // Escribe una función que tome dos números como argumentos y devuelva su producto.
@@ -15,16 +15,16 @@ let dividir = (a, b) => a / b;
 console.log(dividir(785, 45));
 
 // Escribe una función que tome un número base y un exponente y devuelva el resultado de elevar el número base a la potencia dada.
-
-
+let potencia = (base, exponente) => base ** exponente;
+console.log(potencia(4, 7));
 
 // Escribe una función que tome dos números como argumentos y devuelva el resto de la división del primer número entre el segundo.
-
-
+let resto = (a, b) => a / b;
+console.log(resto(45, 7));
 
 // Escribe una función que tome un número como argumento y devuelva su raíz cuadrada.
-
-
+let raizCuadrada  = (num) => Math.sqrt(num); //es una función incorporada en JavaScript que calcula la raíz cuadrada de un número.
+console.log(raizCuadrada(64));
 
 // Escribe una función que tome un número como argumento y devuelva su valor absoluto.
 
