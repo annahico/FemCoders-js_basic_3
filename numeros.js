deb799afc7df42e42246e670be7e48ac27105350
annahico <annahico@gmail.com>
2025-11-20 15:00:24 +0100

    feat: finished 4/10 activities

--- numeros.js
+++ numeros.js
@@ -1,18 +1,18 @@
 // Escribe una función que tome dos números como argumentos y devuelva su suma.
-
-
+let sumar = (a , b) => a + b;
+console.log(sumar ( 4 , 9));
 
 // Escribe una función que tome dos números como argumentos y devuelva su resta.
-
-
+let restar = (a , b) => a - b;
+console.log(restar(7, 5));
 
 // Escribe una función que tome dos números como argumentos y devuelva su producto.
-
-
+let multiplicar = (a, b) => a * b;
+console.log(multiplicar(56, 65));
 
 // Escribe una función que tome dos números como argumentos y devuelva su división.
-
-
+let dividir = (a, b) => a / b;
+console.log(dividir(785, 45));
 
 // Escribe una función que tome un número base y un exponente y devuelva el resultado de elevar el número base a la potencia dada.
 
