ca8b002ba3ad6c1e0036c292af1d9656839c92de
annahico <annahico@gmail.com>
2025-11-20 14:52:45 +0100

    first commit

---
+++ numeros.js
@@ -0,0 +1,39 @@
+// Escribe una función que tome dos números como argumentos y devuelva su suma.
+
+
+
+// Escribe una función que tome dos números como argumentos y devuelva su resta.
+
+
+
+// Escribe una función que tome dos números como argumentos y devuelva su producto.
+
+
+
+// Escribe una función que tome dos números como argumentos y devuelva su división.
+
+
+
+// Escribe una función que tome un número base y un exponente y devuelva el resultado de elevar el número base a la potencia dada.
+
+
+
+// Escribe una función que tome dos números como argumentos y devuelva el resto de la división del primer número entre el segundo.
+
+
+
+// Escribe una función que tome un número como argumento y devuelva su raíz cuadrada.
+
+
+
+// Escribe una función que tome un número como argumento y devuelva su valor absoluto.
+
+
+
+// Escribe una función que tome un número decimal como argumento y devuelva el número redondeado al entero más cercano.
+
+
+
+// Escribe una función que genere y devuelva un número aleatorio entre 0 y 1.
+
+
